14965e783d84849cd020e139a943b55f63ad35fd
Anna Zak <zak.anna.94@gmail.com>
2020-12-31 12:18:31 +0100

    cleaned code

--- src/recognition.js
+++ src/recognition.js
@@ -1,25 +1,20 @@
 export default class SpeechRecognition {
     async recognise() {
         return new Promise(resolve => {
-            var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-            var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
-
-            let result;
-
+            const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
             const recognition = new SpeechRecognition();
+
             recognition.continuous = false;
             recognition.lang = 'pl-PL';
             recognition.interimResults = false;
             recognition.maxAlternatives = 1;
 
-            recognition.start();
-
-            recognition.onresult = function(e) {
+            recognition.onresult = function(event) {
                 recognition.stop();
-                result = e.results[0][0].transcript;
-                resolve(result);
+                resolve(event.results[0][0].transcript);
             }
 
+            recognition.start();
         });
     }
 }
