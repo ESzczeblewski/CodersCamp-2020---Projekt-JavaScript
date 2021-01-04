e964a2aa876d66ea71d0eb7d2060cb075f7cc8da
Cidebur <mcibor96@gmail.com>
2021-01-04 19:32:20 +0100

    index.js now changes styling of a button

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import Engine from './app/engine.js';
 const button = document.querySelector('.btnSpeak');
 
 const engine = new Engine();
-button.addEventListener('click', engine.init);
+
+button.addEventListener('click', buttonOnClick);
+
+
+
+function buttonOnClick() {
+    button.classList.add("lisining");
+    engine.init.bind(engine);
+}
