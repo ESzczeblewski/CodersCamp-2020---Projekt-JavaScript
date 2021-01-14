9aa45c8f532237ef70b3becd8304cad444ce42d8
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-01-14 18:23:22 +0100

    use axios to request data from Wikipedia API

--- src/app/wikiAPI.js
+++ src/app/wikiAPI.js
@@ -1,22 +1,33 @@
+const axios = require('axios').default;
+
 export default class wikiAPI {
     constructor() {
     }
     
     async processPhrase (phrase) {
-        this.phrase = phrase.trim(); // czy muszę trimować?
-        this.wikiAnswer = await this.wikiRequest(phrase);
-        console.log(this.wikiAnswer);
-        return this.wikiAnswer;
+        this.phrase = phrase;
+        await this.wikiRequest(this.phrase);
+        console.log(`w processPhrase:`);
+        console.log(this.wikiResults);
+        return this.wikiResults;
     }
 
     async wikiRequest(phrase) {
         const requestURL = `https://pl.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${phrase}`;
-        const response = await fetch(requestURL);
-        // if (!response.ok) {
-        //     throw Error(response.statusText);
-        // }
-        const jsonWikiResults = await response.json();
-        console.log(jsonWikiResults);
-        return jsonWikiResults;
+        await axios.get(requestURL)
+            .then((response) => {
+                if(response.data.query.searchinfo.totalhits === 0) {
+                    this.wikiResults = 'Na Twoje pytanie ludzkość nie znalazła jeszcze odpowiedzi. Spróbuj zapytać o coś innego!';
+                    console.log(this.wikiResults);
+                    return this.wikiResults;
+                } else {
+                    this.wikiResults = response.data.query.search;
+                    console.log(`w wikiRequest:`);
+                    console.log(this.wikiResults);
+                    return this.wikiResults;
+                }
+            }, (error) => {
+                console.log(error);
+            });
     }
 }
