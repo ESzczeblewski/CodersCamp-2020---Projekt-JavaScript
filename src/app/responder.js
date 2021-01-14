a55ad3f7bee2cd4a11fd6cb945d2c491beb07eee
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-01-14 21:59:43 +0100

    performs wikpedia search when question asked

--- src/app/responder.js
+++ src/app/responder.js
@@ -1,17 +1,40 @@
+import wikiAPI from './wikiAPI.js';
+
 export default class Responder {
     constructor() {
-        this._defaultResponse = "nie rozumiem"
+        this._defaultResponse = "nie rozumiem";
         this._expressionDictionary = [
             ["dzień dobry", "witaj"],
             ["dobry wieczór"],
             ["do widzenia", "do zobaczenia", "na razie", "żegnaj"],
             ["cześć", "siema", "elo"]
-        ]
+        ];
+        this._wikiQuestions = [ 
+            "co to jest",
+            "co to znaczy",
+            "co to",
+            "czym są",
+            "kto to jest", 
+            "kto to", 
+            "kim jest",
+            "jaki jest", 
+            "jaka jest", 
+            "jakie jest", 
+            "jakie są"
+        ];
+        this.wikipedia = new wikiAPI();
     }
-    respondTo(msg) {
+    async respondTo(msg) {
         for (const expressionArr of this._expressionDictionary){
             if (expressionArr.some(expression => msg.includes(expression))){
                 return expressionArr[this._randomInt(expressionArr.length)];
+            }
+        }
+        for (const wikiQuestion of this._wikiQuestions){
+            if(msg.includes(wikiQuestion)) {
+                this.wikiSearchPhrase = msg.replace(`${wikiQuestion} `, "");
+                this.wikiAnswersArr = await this.wikipedia.processPhrase(this.wikiSearchPhrase); // zwraca tablicę obiektów, trzeba przerobić, by zwracała właściwość '.snippet' pierwszego obiektu do czytania
+                return this.wikiAnswersArr;  
             }
         }
         return this._defaultResponse;
--- src/app/wikiAPI.js
+++ src/app/wikiAPI.js
@@ -7,23 +7,18 @@ export default class wikiAPI {
     async processPhrase (phrase) {
         this.phrase = phrase;
         await this.wikiRequest(this.phrase);
-        console.log(`w processPhrase:`);
-        console.log(this.wikiResults);
         return this.wikiResults;
     }
 
     async wikiRequest(phrase) {
-        const requestURL = `https://pl.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${phrase}`;
+        const requestURL = `https://pl.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=5&srsearch=${phrase}`;
         await axios.get(requestURL)
             .then((response) => {
                 if(response.data.query.searchinfo.totalhits === 0) {
                     this.wikiResults = 'Na Twoje pytanie ludzkość nie znalazła jeszcze odpowiedzi. Spróbuj zapytać o coś innego!';
-                    console.log(this.wikiResults);
                     return this.wikiResults;
                 } else {
                     this.wikiResults = response.data.query.search;
-                    console.log(`w wikiRequest:`);
-                    console.log(this.wikiResults);
                     return this.wikiResults;
                 }
             }, (error) => {
