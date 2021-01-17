4536343e261d25244de085b251504cb7d405342f
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-01-17 16:21:10 +0100

    assistant listens continuously, & prettified code in recognition.js

--- src/app/recognition.js
+++ src/app/recognition.js
@@ -1,56 +1,54 @@
 class Recognition {
+
 	constructor(settings = {}) {
-	this.settings = settings;
-	this.speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-	this.recognition = new this.speechRecognition();
-	this.result = '';
-	this.recording = true;
-	this.listening = false;
-	this.startRecognition();
-}
-
-	startRecognition() {
-
-	this.recognition.lang = this.settings.lang || 'pl-PL';
-	this.recognition.continuous = false;
-	this.recognition.interimResults = false;
-
-	this.recognition.addEventListener('result', event => {
-
-		const { resultIndex } = event;
-		const recognized = event.results[resultIndex][0].transcript.trim();
-
-		if(this.recording) {
-			this.result = recognized.toLowerCase();
-			return;
-		}
-
+		this.settings = settings;
+		this.speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+		this.recognition = new this.speechRecognition();
 		this.result = '';
-
-	});
-
-}
-
-onRecognitionResult(callback) {
-	this.recognition.addEventListener('end', () => {
-		if (this.recording && this.result !== '') callback(this.result);
-	})
-}
-
-listen(state){
-			state ? this.listening = true : this.listening = false;
-			this.result = '';
+		this.recording = true;
+		this.listening = false;
+		this.startRecognition();
 	}
 
-startRecording() {
-	this.recording = true;
-	this.recognition.start();
-}
+	startRecognition() {
+		this.recognition.lang = this.settings.lang || 'pl-PL';
+		this.recognition.continuous = false;
+		this.recognition.interimResults = false;
 
-stopRecording() {
-	this.recording = false;
-	this.recognition.abort();
-}
+		this.recognition.addEventListener('result', event => {
+			const { resultIndex } = event;
+			const recognized = event.results[resultIndex][0].transcript.trim();
+
+			if (this.recording) {
+				this.result = recognized.toLowerCase();
+				return;
+			}
+
+			this.result = '';
+		});
+	}
+
+	onRecognitionResult(callback) {
+		this.recognition.addEventListener('end', () => {
+			if (this.recording && this.result !== '') callback(this.result);
+			else this.recognition.start();
+		});
+	}
+
+	listen(state) {
+			state ? this.listening = true : this.listening = false;
+			this.result = '';
+		}
+
+	startRecording() {
+		this.recording = true;
+		this.recognition.start();
+	}
+
+	stopRecording() {
+		this.recording = false;
+		this.recognition.abort();
+	}
 
 }
 
