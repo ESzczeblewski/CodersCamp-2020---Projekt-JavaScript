24f72002b100da2bce8a035a1b1619f37ed6cace
Cidebur <mcibor96@gmail.com>
2021-01-18 21:09:48 +0100

    quick refactoring

--- test/wikiAPI.test.js
+++ test/wikiAPI.test.js
@@ -1,48 +1,45 @@
 import wikiAPI from '../src/app/wikiAPI';
 import axios from 'axios';
 
+const wiki = new wikiAPI();
+
 jest.mock('axios');
 
-test('check if found phrase', () => {
-    const searchInfo = { totalhits: 1 }
-    const query = { search: 'wikiResult', searchinfo: searchInfo };
-    const data = { query: query }
-    const resp = { data: data };
-    console.log(resp)
-    axios.get.mockResolvedValue(resp);
-    const wiki = new wikiAPI();
-    wiki._phrase = '';
-    return wiki._wikiRequest().then(data => expect(data).toEqual(resp.data.query.search));
-})
+const mockWikiResponse = (mySearch, myTotalhits) => {
+    return {data: {query: {search: mySearch, searchinfo: {totalhits: myTotalhits}}}}
+}
 
+test('check if found phrase', () => {
+    
+    const result = "wikiresult";
+    const totalHits = 1;
+    const resp = mockWikiResponse(result, totalHits)
+
+    axios.get.mockResolvedValue(resp);
+    return wiki._wikiRequest().then(data => expect(data).toEqual(result));
+})
 
 test('check for no phrase', () => {
-    const searchInfo = { totalhits: 0 }
-    const query = { search: null, searchinfo: searchInfo };
-    const data = { query: query }
-    const resp = { data: data };
-    console.log(resp)
+    const resp = mockWikiResponse(null, 0);
+
     axios.get.mockResolvedValue(resp);
-    const wiki = new wikiAPI();
-    wiki._phrase = '';
     return wiki._wikiRequest().then(data => expect(data).toEqual(''));
 })
+
 test(' check processPhrase function', () => {
-    const searchInfo = { totalhits: 1 }
-    const query = { search: 'wikiResult', searchinfo: searchInfo };
-    const data = { query: query }
-    const resp = { data: data };
-    console.log(resp)
+    const result = "wikiresult";
+    const totalHits = 1;
+    const phrase = "Piłsudski";
+    const resp = mockWikiResponse(result, totalHits)
     axios.get.mockResolvedValue(resp);
-    const wiki = new wikiAPI();
-    const phrase = '';
+    
     return wiki.processPhrase(phrase).then(data => expect(data).toEqual(resp.data.query.search));
 })
 
 test('Invalid response', async () => {
-    const wiki = new wikiAPI();
-    const phrase = 'auto'
-    axios.get.mockRejectedValue('błąd');
-    return wiki.processPhrase(phrase).catch(data => expect(data).toBe('błąd'));
+    const phrase = 'auto';
+    const error = 'błąd';
+    axios.get.mockRejectedValue(error);
+    return wiki.processPhrase(phrase).catch(data => expect(data).toBe(error));
 
 });
